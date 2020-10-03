7c1c9a150ae6fcadc82bc7fa63319c02e05e1444
睿达人民，永不认输！ <xxy3429924750@126.com>
2020-10-03 21:02:12 +0800

    悬浮窗

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tkinter.messagebox import *
 #zhushi
 
 root = Tk()
-
+root.title('')
 root.wm_attributes("-alpha", 0.1)        # 透明度(0.0~1.0)
 root.wm_attributes("-toolwindow", True)  # 置为工具窗口(没有最大最小按钮)
 root.wm_attributes("-topmost", True)     # 永远处于顶层
